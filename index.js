7f4b16b32f9551037a66d01ea2976683e15c589e
Bob Rockers <bob@rockerssoft.org>
2018-03-10 10:29:33 -0600

    Moving on to functions

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 
 var beers = 99;
 
+function printVerse(num){
+
+		var name = "bottles";
+		if(num === 1) { name = "bottle"; }
+
+		console.log(num + " " + name + " of beer on the wall");
+		console.log(num + " " + name + " of beer");
+		console.log("You take one down, you pass it around.");
+		num--;
+		console.log(num + " bottles of beer on the wall\n");
+		return num;
+}
+
 // repeat the section 99 times
-while(beers > 0) {
-	
-	if(beers === 1) {
-		console.log(beers + " bottle of beer on the wall");
-		console.log(beers + " bottle of beer");
-		console.log("You take one down, you pass it around.");
-		beers--;
-		console.log("No more bottles of beer on the wall!!!  GO buy more beer.\n");
-	} else {
-		console.log(beers + " bottles of beer on the wall");
-		console.log(beers + " bottles of beer");
-		console.log("You take one down, you pass it around.");
-		beers--;
-		console.log(beers + " bottles of beer on the wall\n");
-		// beers = beers - 1;
-	}
+while(beers > 0) {	
+		// console.log("No more bottles of beer on the wall!!!  GO buy more beer.\n");
+		beers = printVerse(beers);
 }
